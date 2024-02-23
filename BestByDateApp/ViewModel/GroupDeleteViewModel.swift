27bb87d1d4bbedaa1c8763409ce5189f151dc954
ryo yaguchi <cocoanthi89@gmail.com>
2024-02-24 01:04:07 +0900

    ViewModelファイル追加

---
+++ BestByDateApp/ViewModel/GroupDeleteViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GroupDeleteViewModel.swift
+//  BestByDateApp
+//
+//  Created by 矢口諒 on 2024/02/24.
+//
+
+import Foundation
---
+++ BestByDateApp/ViewModel/GroupHistoryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GroupHistoryViewModel.swift
+//  BestByDateApp
+//
+//  Created by 矢口諒 on 2024/02/24.
+//
+
+import Foundation
