098a472ac7380703be175e64dba54acb9cb326a2
ryo yaguchi <cocoanthi89@gmail.com>
2024-02-24 01:18:13 +0900

    repositoryファイル追加

---
+++ BestByDateApp/Data/Repository/BestByDateRepository.swift
@@ -0,0 +1,28 @@
+//
+//  BestByDateRepository.swift
+//  BestByDateApp
+//
+//  Created by 矢口諒 on 2024/02/24.
+//
+
+import Foundation
+
+struct BestByDateRepository {
+    
+    func register() {
+        // TODO: 登録処理
+    }
+
+    func fetch() {
+        // TODO: 取得処理
+    }
+    
+    func update() {
+        // TODO: 更新処理
+    }
+    
+    func delete() {
+        // TODO: 削除処理
+    }
+    
+}
